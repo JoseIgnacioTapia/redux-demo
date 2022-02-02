def7ad51d249b8e9af3d33ac932bb17148c8b73f
JoseIgnacioTapia <josignaciot@gmail.com>
2022-02-02 18:48:35 -0300

    Fixing & Finalizing the Fetching Logic

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -19,7 +19,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       uiActions.showNotification({
         status: 'error',
@@ -45,7 +50,10 @@ export const sendCartData = cart => {
         'https://cart-redux-652c9-default-rtdb.firebaseio.com/cart.json',
         {
           method: 'PUT',
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
